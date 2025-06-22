0a6228cdc01543ecaaabe1d79775b6cd8f683be2
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-06-22 13:05:18 +0200

    feature: adds consumer logic to execute a task from queue

--- src/main/java/com/kwizera/threads/Consumer.java
+++ src/main/java/com/kwizera/threads/Consumer.java
@@ -21,5 +21,23 @@ public class Consumer implements Runnable {
         // check the queue for a task
         // if not empty, take() task process it, update its state, log it
         // if empty, block
+        while (true) {
+            try {
+                Task task = queue.take();
+                processTask(task);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private void processTask(Task task) {
+        try {
+            tasksState.put(task.getId(), TaskStatus.PROCESSING);
+            Thread.sleep(150);
+            tasksState.put(task.getId(), TaskStatus.COMPLETED);
+        } catch (InterruptedException e) {
+            tasksState.put(task.getId(), TaskStatus.FAILED);
+        }
     }
 }
